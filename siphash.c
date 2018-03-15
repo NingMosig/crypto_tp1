e7805b7b4a504d0177c114e95e53d80a9fc33861
Etienne Bontemps <etienne.bontemps@grenoble-inp.org>
2018-03-15 15:16:19 +0100

    fixed chunks (silly loop missing)

--- siphash.c
+++ siphash.c
@@ -10,31 +10,31 @@ const int c=2;
 uint64_t v[4];
 // args
 uint64_t k[2];
-uint8_t m[] = {0x0, 0x1, 0x3, 0x4, 0x5, 0x6, 0x7};
+// uint8_t m[] = {0x0, 0x1, 0x3, 0x4, 0x5, 0x6, 0x7};
+//? 0xff is uint8_t
+uint8_t m[] = {0x00, 0x01, 0x03, 0x04, 0x05, 0x06, 0x07};
 unsigned mlen;
 
 
 void sipround() {
-    //
+    // para
     v[0] += v[1];
     v[1] <<= 13;
     v[1] ^= v[0];
-    //
+    v[0] <<= 32;
+    // para
     v[2] += v[3];
     v[3] <<= 16;
     v[3] ^= v[2];
-    //
-    v[0] <<= 32;
-    //
+    // para
     v[2] += v[1];
     v[1] <<= 17;
     v[1] ^= v[2];
-    //
+    v[2] <<= 32;
+    // para
     v[0] += v[3];
     v[3] <<= 21;
     v[3] ^= v[0];
-    //
-    v[2] <<= 32;
 }
 
 
@@ -43,6 +43,7 @@ uint64_t siphash_2_4(uint64_t k[2], uint8_t *m, unsigned mlen) {
     uint8_t w = (mlen + 1)%8 ? (mlen + 1)/8 + 1 : (mlen + 1) / 8;
     uint64_t words[w];
     int i;
+    int j;
 
     // initialization
     for (i = 0; i<4; i++) {
@@ -51,11 +52,24 @@ uint64_t siphash_2_4(uint64_t k[2], uint8_t *m, unsigned mlen) {
 
     // compression
     for (i = 0; i<w-1; i++) {
-        words[i] = m[i];
+        words[i] = 0; // needed ? memset instead ?
+        for (j = 0; j<8; j++) { // each word is 8 bytes long
+            words[i] += (m[8*i+j] << 8*(j-1));
+        }
         v[3] = v[3] ^ words[i];
     }
-    words[w-1] = *(m + 8*w);
+    // what if m[w-1] is empty ?
+    // what if only part of m is useful ?
+    // we need to stop when m stops 00 0f ff ff | ff ff ff ff | ff ff ff ff |...
+    //                                  ^ stop here
+    //                              ^ last word  ^ word        ^ word
+    words[w-1] = 0;
+    for (j = 0; j<(mlen%8); j++) { // stop when m ends if mlen is a multiple of 8 then nothing will be added here (modulo) the case j<7 is not a problem because here the last pack of m should not completely fill the cell of words
+        words[w-1] += (m[8*(w-1)+j] << 8*(j-1));
+    }
+    // the last cell of words is filled here:
     words[w-1] += (mlen%256 << 56);
+    // printf("%i\n", words[w-1]);
     v[3] = v[3] ^ words[w-1];
 
     // c siprounds
@@ -82,8 +96,10 @@ void main(int *argc, char **argv) {
     mlen = 8;
     k[0] = 0x0706050403020100;
     k[1] = 0x0f0e0d0c0b0a0908;
+    // k[0] = 0;
+    // k[1] = 0;
 
-    printf("siphash_2_4(k, m, mlen) = 0x%16x\n", siphash_2_4(k, m, mlen));
+    printf("siphash_2_4(k, m, mlen) = %p\n", siphash_2_4(k, m, mlen));
 
     return;
 }
